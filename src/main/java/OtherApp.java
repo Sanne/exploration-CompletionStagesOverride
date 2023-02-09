21b0eaa67a3802ec95e57ae56e8456507d2337ca
Sanne Grinovero <sanne@hibernate.org>
2023-02-09 08:53:35 +0100

    Clarify the code a bit

--- src/main/java/OtherApp.java
+++ src/main/java/OtherApp.java
@@ -1,10 +1,16 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
+/**
+ * Experiments with the twisted semantics of {@link CompletableFuture#supplyAsync(Supplier)}
+ */
 public class OtherApp {
 
-	private static final boolean wait = false;
+	//Switch this constant to experiment with different behaviours
+	private static final boolean wait = true;
 
 	public static void main(String[] args) {
+		System.out.println("Method 'main' is running on thread " + threadName());
 		VeryParallelExecutor executor = new VeryParallelExecutor( 4 );
 
 //		OneOffDelegatingExecutor taskControl = new OneOffDelegatingExecutor( executor );
@@ -12,7 +18,7 @@ public class OtherApp {
 		CompletableFuture comp = CompletableFuture.supplyAsync(
 				OtherApp::asyncGenerateInteger,
 				executor )
-				.thenApply( OtherApp::incrementInput )
+				.thenApply( OtherApp::incrementInput )//Which thread is running this? : timing dependent!
 				.thenAccept( OtherApp::printOutput );
 
 //		taskControl.runHeldTasks();
@@ -21,9 +27,14 @@ public class OtherApp {
 
 		executor.shutdownAndWait();
 	}
+
+	private static String threadName() {
+		return '['+Thread.currentThread().getName()+']';
+	}
+
 	private static Integer asyncGenerateInteger() {
 		if (wait) sleep2seconds();
-		System.out.println( "asyncGenerateInteger: " + Thread.currentThread().getName() );
+		System.out.println( "asyncGenerateInteger is running on: " + threadName() );
 		return 1;
 	}
 
@@ -37,12 +48,11 @@ public class OtherApp {
 	}
 
 	private static Integer incrementInput(Integer i) {
-		System.out.println( "incrementInput: " + Thread.currentThread().getName() );
+		System.out.println( "incrementInput running on: " + threadName() );
 		return i + 1;
 	}
 
 	private static void printOutput(Integer i) {
-		System.out.println( "printOutput: " + Thread.currentThread().getName() );
-		System.out.println( "result is: " + i );
+		System.out.println( "printOutput running on: " + threadName() + "; result is: " + i );
 	}
 }
